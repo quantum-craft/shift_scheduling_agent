6b4b3983700b4d318e9972978b17789cb31cba0e
kevin_wang <kevin_wang@mayohr.com>
2025-06-18 09:17:15 +0800

    feature: 班別清單 API

--- apitest.py
+++ apitest.py
@@ -2,11 +2,12 @@ import os
 import asyncio
 from dotenv import load_dotenv
 
+from uuid import UUID
 from hrm.webapi.unified.employees import employees as emp_api
 from hrm.webapi.unified.groups.customs import groups as customgroup_api
 from hrm.webapi.unified.groups.customs import employees as customgroupemp_api
 from hrm.webapi.unified.groups.customs.shift_schedule_sections import requieements as groupreq_api
-
+from hrm.webapi.unified.shifts import shifts as shifts_api
 
 if __name__ == "__main__":
     async def main():
@@ -16,6 +17,7 @@ if __name__ == "__main__":
         # print(f"employees.get_my_employee_info {await emp_api.get_my_employee_info(token)}")
         # print(f"groups.get_custom_group_info {await customgroup_api.get_custom_group_info(token)}")
         # print(f"groups.get_custom_group_info {await customgroupemp_api.get_employees_info(token)}")
-        print(f"groupreq_api.get_requirements {await groupreq_api.get_requirements(token)}")
+        # print(f"groupreq_api.get_requirements {await groupreq_api.get_requirements(token)}")
+        print(f"shifts_api.get_shifts {await shifts_api.get_shifts(token,[ UUID('214ff328-eb67-4548-a298-1cc4665c810b')])}")
 
     asyncio.run(main())
