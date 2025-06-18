932e07186995401ef6d1fff1e7c5e5c3988f7061
kevin_wang <kevin_wang@mayohr.com>
2025-06-18 10:57:43 +0800

    feature: 員工套用班別 API

--- apitest.py
+++ apitest.py
@@ -9,7 +9,8 @@ from hrm.webapi.unified.groups.customs import employees as customgroupemp_api
 from hrm.webapi.unified.groups.customs.shift_schedule_sections import requieements as groupreq_api
 from hrm.webapi.unified.shifts import shifts as shifts_api
 from hrm.webapi.unified.shiftschedule import shiftschedule as shiftschedule_api
-from hrm.webapi.unified.shiftschedule import cycles as shiftschedulecycles_api
+from hrm.webapi.unified.shiftschedule import cycles as shiftschedulecycle_api
+from hrm.webapi.unified.employeeschedulingrulesettings import shifts as employeeshift_api
 
 if __name__ == "__main__":
     async def main():
@@ -22,5 +23,6 @@ if __name__ == "__main__":
         # print(f"groupreq_api.get_requirements {await groupreq_api.get_requirements(token)}")
         # print(f"shifts_api.get_shifts {await shifts_api.get_shifts(token,[ UUID('214ff328-eb67-4548-a298-1cc4665c810b')])}")
         # print(f"shiftschedule_api.get_shiftschedules {await shiftschedule_api.get_shiftschedules(token,[ UUID('58244e12-47a3-4473-80cb-4e48daeb8c74')])}")
-        print(f"shiftschedulecycles_api.get_cycles {await shiftschedulecycles_api.get_cycles(token,[ UUID('58244e12-47a3-4473-80cb-4e48daeb8c74')])}")
+        # print(f"shiftschedulecycle_api.get_cycles {await shiftschedulecycle_api.get_cycles(token,[ UUID('58244e12-47a3-4473-80cb-4e48daeb8c74')])}")
+        print(f"employeeshift_api.get_employee_shifts {await employeeshift_api.get_employee_shifts(token,[UUID('7e9817f6-44d5-4836-a204-1116481d76fc')],'2025-05-29','2025-05-29')}")
     asyncio.run(main())
---
+++ hrm/webapi/unified/employeeschedulingrulesettings/shifts.py
@@ -0,0 +1,52 @@
+"""HRM 的 員工套用班別相關 API"""
+
+import httpx
+from typing import List, Optional
+from pydantic import BaseModel, Field
+from datetime import date
+from uuid import UUID
+from hrm.webapi.unified.response import WebAPIResponse
+from hrm.webapi.unified.urlutils import *
+from http import HTTPStatus
+from datetime import time
+
+
+class EmployeeShiftAssignmentViewModel(BaseModel):
+    """員工班別指派資料"""
+    employee_id: UUID = Field(..., alias="employeeId", description="員工 ID")
+    effective_date: date = Field(..., alias="effectiveDate", description="生效日期")
+    shift_id: UUID = Field(..., alias="shiftId", description="班別 ID")
+
+
+
+async def get_employee_shifts(token: str, ids: Optional[List[UUID]], startDate: str, endDate: str) -> WebAPIResponse[List[EmployeeShiftAssignmentViewModel]]:
+    """
+    取得員工套用班別清單.
+
+    :raises:
+        HTTPStatusError: 驗證失敗
+
+    Returns:
+        員工套用班別清單
+    """
+
+    async with httpx.AsyncClient() as client:
+        my_headers = {"Authorization": token}
+        base_url = get_hrm_tool_full_endpoint(
+            "api/employee-scheduling-rule-settings/shifts")
+
+        date_condition = f"?startDate={startDate}&endDate={endDate}"
+        if ids:
+            query = "&".join([f"ids={str(i)}" for i in ids])
+            full_url = f"{base_url}?{date_condition}&{query}"
+        else:
+            full_url = f"{base_url}?{query}"
+
+        response = await client.get(full_url, headers=my_headers)
+        if response.status_code != HTTPStatus.OK:
+            print(f"請求失敗，狀態碼: {response.status_code}")
+            response.raise_for_status()
+        print(response.json())
+        response_model = WebAPIResponse[List[EmployeeShiftAssignmentViewModel]](
+            **response.json())
+        return response_model
