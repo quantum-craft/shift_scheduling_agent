a694e326582206bb6717dd056ac4db1f5a9ecb8e
Henry_Chen <henry_chen@mayohr.com>
2025-06-18 18:43:59 +0800

    [HenryChen] remove test code

--- agent/implementation.py
+++ agent/implementation.py
@@ -80,19 +80,19 @@ compiled_agent = agent.compile()
 
 
 # Test codes
-if __name__ == "__main__":
-    # config = {
-    #     "configurable": {
-    #         # "general_model": "OpenAI-GPT-4o",
-    #         # "general_agent_system_prompt": "請以繁體中文回答",
-    #     }
-    # }
+# if __name__ == "__main__":
+# config = {
+#     "configurable": {
+#         # "general_model": "OpenAI-GPT-4o",
+#         # "general_agent_system_prompt": "請以繁體中文回答",
+#     }
+# }
 
-    for chunk in compiled_agent.stream(
-        {
-            "messages": [HumanMessage(content="排六月的班表")],
-        },
-        # config=config,
-        stream_mode="values",
-    ):
-        chunk["messages"][-1].pretty_print()
+# for chunk in compiled_agent.stream(
+#     {
+#         "messages": [HumanMessage(content="排六月的班表")],
+#     },
+#     # config=config,
+#     stream_mode="values",
+# ):
+#     chunk["messages"][-1].pretty_print()
